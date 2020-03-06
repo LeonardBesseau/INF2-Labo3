0fd2a981261673acdd7635f2d2f07e499158ab87
LeonardBesseau <leonard.besseau@heig-vd.ch>
2020-03-06 15:52:46 +0100

    modified gitignore

--- Carte.cpp
+++ Carte.cpp
@@ -3,3 +3,24 @@
 //
 
 #include "Carte.h"
+
+
+
+Carte::Carte(unsigned short familly, unsigned short member) :familly(familly), member(member){}
+
+unsigned short Carte::getFamilly() const {
+    return familly;
+}
+
+unsigned short Carte::getMember() const {
+    return member;
+}
+
+std::string Carte::toString() const {
+    return std::to_string(familly)+getMemberName();
+}
+
+std::string Carte::getMemberName() const{
+    string
+    return std::__cxx11::string();
+}
--- Carte.h
+++ Carte.h
@@ -1,13 +1,35 @@
-//
-// Created by leonard on 3/6/20.
-//
+/* ---------------------------
+Laboratoire : 02
+Fichier : Carte.h
+Auteur(s) : Besseau
+Date : 06-03-2020
 
+But :
+
+Remarque(s) :
+
+Compilateur : gcc version 7.4.0
+
+--------------------------- */
 #ifndef LABO3_CARTE_H
 #define LABO3_CARTE_H
 
+#include <string>
 
 class Carte {
+public:
+    Carte(unsigned short familly, unsigned short member);
 
+    unsigned short getFamilly() const;
+
+    unsigned short getMember() const;
+
+    std::string toString() const;
+
+private:
+    std::string getMemberName() const;
+    unsigned short familly;
+    unsigned short member;
 };
 
 
