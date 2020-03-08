aca8bdd275015d9351d8ddaafedd5242345e4761
LeonardBesseau <leonard.besseau@heig-vd.ch>
2020-03-08 18:54:15 +0100

    modified carte

--- Partie.cpp
+++ Partie.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "Partie.h"
+
+Partie::Partie(unsigned nbPlayer, unsigned nbFamilly, unsigned cardsPerFamily, unsigned cardsPerPlayer): cardPerFamilly(cardsPerFamily) {
+    std::vector<Carte> cards(nbFamilly*cardsPerFamily);
+    for (int i = 1; i <= nbFamilly; ++i) {
+
+    }
+    player = std::vector<Joueur>(nbPlayer);
+}
+
+unsigned int Partie::getTurns() const {
+    return Turns;
+}
+
+const std::vector<Carte> &Partie::getStack() const {
+    return Stack;
+}
+
+const std::vector<Joueur> &Partie::getPlayerList() const {
+    return player;
+}
+
+void Partie::displayStack() {
+
+}
+
+
+
+
--- Partie.h
+++ Partie.h
@@ -5,9 +5,28 @@
 #ifndef LABO3_PARTIE_H
 #define LABO3_PARTIE_H
 
+#include "Carte.h"
+#include "Joueur.h"
+#include <vector>
+#include <array>
 
 class Partie {
+public:
+    Partie(unsigned nbPlayer,unsigned  nbFamilly, unsigned cardsPerFamily, unsigned cardsPerPlayer );
 
+    unsigned int getTurns() const;
+
+    const std::vector<Carte> &getStack() const;
+
+    const std::vector<Joueur> &getPlayerList() const;
+
+    void displayStack();
+
+private:
+    unsigned Turns;
+    unsigned cardPerFamilly;
+    std::vector<Carte> Stack;
+    std::vector<Joueur> player;
 };
 
 
